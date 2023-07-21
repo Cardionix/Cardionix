e5858b13c3d37655de7655c3276d31eb2956b14e
YeaMerci <113313229+YeaMerci@users.noreply.github.com>
2023-07-21 20:08:55 +0300

    feat: added CardioDataModule class

--- lightning/datamodule/datamodule.py
+++ lightning/datamodule/datamodule.py
@@ -2,4 +2,79 @@
 Docstring
 """
 
-__all__ = ["CardioDatamodule"]
+__all__ = ["CardioDataModule"]
+
+
+import pytorch_lightning as pl
+from torch.utils.data import DataLoader
+from ..dataset import CardioDataset
+
+
+class CardioDataModule(pl.LightningDataModule):
+    def __init__(self, data_params: BaseModel):
+        super().__init__()
+        self.__dataset_params = data_params.dataset
+        self.__datamodule_params = data_params.datamodule
+        self.__transform_params = data_params.transform
+
+        self.__num_subsets = len(self.__dataset_params.split_ratio)
+        self.__data_source = "https://www.kaggle.com/datasets/mersico/dangerous-heartbeat-dataset-dhd"
+
+        self.__data_train = None
+        self.__data_val = None
+        self.__data_test = None
+
+    def prepare_data(self) -> None:
+        print(
+            f"Warning! This method does not load the data to process it. "
+            f"You have to download the data yourself. Data source: {self.__data_source}"
+        )
+
+    def setup(self, stage: str = None) -> None:
+        if stage == "train" or stage is None:
+            self.__data_train = CardioDataset(
+                self.__dataset_params,
+                self.__transform_params,
+                stage="train"
+            )
+
+        if stage == "val" or stage is None:
+            self.__data_val = CardioDataset(
+                self.__dataset_params,
+                self.__transform_params,
+                stage="val"
+            )
+
+        if self.__num_subsets == 3 and (stage == "test" or stage is None):
+            self.__data_test = CardioDataset(
+                self.__dataset_params,
+                self.__transform_params,
+                stage="test"
+            )
+
+    def train_dataloader(self) -> DataLoader:
+        return DataLoader(
+            dataset=self.__data_train,
+            batch_size=self.__datamodule_params.batch_size,
+            num_workers=self.__datamodule_params.num_workers,
+            shuffle=True,
+            pin_memory=True
+        )
+
+    def val_dataloader(self) -> DataLoader:
+        return DataLoader(
+            dataset=self.__data_val,
+            batch_size=self.__datamodule_params.batch_size,
+            num_workers=self.__datamodule_params.num_workers,
+            shuffle=False,
+            pin_memory=True
+        )
+
+    def test_dataloader(self) -> DataLoader:
+        return DataLoader(
+            dataset=self.__data_test,
+            batch_size=self.__datamodule_params.batch_size,
+            num_workers=self.__datamodule_params.num_workers,
+            shuffle=False,
+            pin_memory=True
+        )
