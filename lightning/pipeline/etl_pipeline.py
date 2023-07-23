6635ab619a15456eaafff02e0ce87da82c628755
YeaMerci <113313229+YeaMerci@users.noreply.github.com>
2023-07-23 15:28:41 +0300

    ref: renamed from transforms.py

--- lightning/pipeline/etl_pipeline.py
+++ lightning/pipeline/etl_pipeline.py
@@ -10,31 +10,39 @@ import torch
 from torch.nn import Module
 import librosa
 
+from ..validate import ETLPipelineParams
+
 
 class ETLPipeline(Module):
     """
     Docstring
     """
     def __init__(self,
-                 transform_params: TransformParams,
+                 etl_pipeline_params: ETLPipelineParams,
                  stage: Literal["train", "val", "test"]
                  ):
         super().__init__()
 
-        self.__n_mfcc = transform_params.n_mfcc
-        self.__duration = transform_params.duration
-        self.__sample_rate = transform_params.sample_rate
-        self.__mono = transform_params.mono
+        self.__n_mfcc = etl_pipeline_params.n_mfcc
+        self.__duration = etl_pipeline_params.duration
+        self.__sample_rate = etl_pipeline_params.sample_rate
+        self.__mono = etl_pipeline_params.mono
         self.__length = self.__duration * self.__sample_rate
         self._stage = stage
 
-    def check_duration(self, waveform: np.ndarray) -> torch.Tensor:
+    def check_duration(self, waveform: np.ndarray) -> np.ndarray:
+        """
+        Docstring
+        """
         duration = librosa.get_duration(y=waveform, sr=self.__sample_rate)
         if duration < self.__duration:
             waveform = librosa.util.fix_length(waveform, size=self.__length)
         return waveform
 
     def extract_mfcc(self, waveform: np.ndarray) -> np.ndarray:
+        """
+        Docstring
+        """
         mfcc = np.mean(
             librosa.feature.mfcc(
                 y=waveform,
@@ -44,6 +52,9 @@ class ETLPipeline(Module):
         return mfcc.reshape([-1, 1])
 
     def forward(self, filepath: str) -> torch.Tensor:
+        """
+        Docstring
+        """
         waveform, _ = librosa.load(
             path=filepath,
             sr=self.__sample_rate,
