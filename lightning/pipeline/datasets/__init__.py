f9d2a0def1d6606db81953d424abcb9c4daab559
YeaMerci <113313229+YeaMerci@users.noreply.github.com>
2023-08-02 13:32:09 +0300

    docs: added docs for datasets package

--- lightning/pipeline/datasets/__init__.py
+++ lightning/pipeline/datasets/__init__.py
@@ -1,5 +1,9 @@
 """
-Docstring
+Description:
+    The package contains implementations of subclasses of the Dataset base class.
+    It is used to split data into parts,
+    transform them and extract features
+    using a third-party ETLPipeline module and load into RAM.
 """
 
 from .classification import *
