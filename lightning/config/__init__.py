b20d2f7fc4f38292417bc5a2c7e7dd84f97353bd
YeaMerci <113313229+YeaMerci@users.noreply.github.com>
2023-08-02 13:17:34 +0300

    docs: added docs for config package

--- lightning/config/__init__.py
+++ lightning/config/__init__.py
@@ -1,5 +1,22 @@
 """
-Docstring
+Description:
+    The package contains modules with classes
+    representing configuration models for various parts of the project.
+    Classes with configurations receive parameters for each module,
+    validate input arguments, and then pass them to the target module.
+
+For example::
+
+    # This class is a datamodule parameter configuration model
+
+    class DataModuleParams(BaseModel):
+        batch_size: Optional[int] = Field(default=4, gt=0)
+        num_workers: Optional[int] = Field(default=2, gt=0)
+
+Where:
+    batch_size -- size of the mini-batch that is issued during the iteration.
+
+    num_workers -- number of threads used when loading data.
 """
 
 from .dataset_config import *
