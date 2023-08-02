b5d9baa7362cbac69978fc4a887f2e7403b13396
YeaMerci <113313229+YeaMerci@users.noreply.github.com>
2023-08-02 13:32:48 +0300

    docs: added docs for segmentation.py module in datasets package

--- lightning/pipeline/datasets/segmentation.py
+++ lightning/pipeline/datasets/segmentation.py
@@ -1,3 +1,8 @@
 """
-Docstring
+Description:
+    The module contains a dataset subclass
+    for segmentation S1 and S2 heartbeat stage by sound.
+
+Note:
+    At the moment, the module is not implemented and is not a priority.
 """
