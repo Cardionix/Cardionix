b7f97081b3d5340f53e94444fffec646a8bcd553
YeaMerci <113313229+YeaMerci@users.noreply.github.com>
2023-08-31 18:39:31 +0300

    ref: renamed datasets

--- cardiosonix/pipeline/datasets/__init__.py
+++ cardiosonix/pipeline/datasets/__init__.py
@@ -5,11 +5,10 @@ transform them and extract features
 using a third-party ETLPipeline module and load into RAM.
 
 Modules:
-    * classification.py - module contains a ``Dataset`` subclass named ``CardioAnomalyDataset``
+    * classify.py - module contains a ``Dataset`` subclass named ``CardioAnomalyDataset``
     for classifying heart rate deviations by sound.
 
-    * segmentation.py - at the moment, the module is not implemented and is not a priority.
+    * segment.py - at the moment, the module is not implemented and is not a priority.
 """
 
-from .classification import *
-
+from .classify import *
