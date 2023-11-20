f30cf8c4f07b56ca593855ffa43a5d41f9ec9b29
YeaMerci <113313229+YeaMerci@users.noreply.github.com>
2023-11-20 08:53:52 +0300

    ref: dataset utils imports and docs

--- cardiosonix/pipeline/datasets/utils/__init__.py
+++ cardiosonix/pipeline/datasets/utils/__init__.py
@@ -1,6 +1,11 @@
-def main():
-    pass
+"""
+This package contains dataset utils that can be used to:
+    - Check process correctness
+    - Parthings of the datasets
+    - Build the datasets
+    - Shuffle the datasets
+    - Deterministic datasets
+    - etc.
+"""
 
-
-if __name__ == "__main__":
-    main()
+from .partitioner import *
