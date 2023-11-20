53594a49f98496073b71986ac0e5ef61dfededd8
YeaMerci <113313229+YeaMerci@users.noreply.github.com>
2023-11-20 09:31:41 +0300

    ref: replaced callbacks

---
+++ callbacks.py
@@ -0,0 +1,30 @@
+"""
+Module containing any callbacks for the CardioTriner class as variables.
+"""
+
+__all__ = ["callbacks"]
+
+from pytorch_lightning.callbacks import ModelCheckpoint, EarlyStopping
+
+
+callbacks = [
+    ModelCheckpoint(
+        dirpath="./checkpoints",
+        filename="epoch={epoch}"
+                 "-precision={val/macro avg/precision:.2f}"
+                 "-recall={val/macro avg/recall:.2f}"
+                 "-f1-score={val/macro avg/f1-score:.2f}",
+        monitor="val/macro avg/f1-score",
+        mode="max",
+        save_top_k=10,
+        auto_insert_metric_name=False,
+        save_weights_only=True
+    ),
+
+    EarlyStopping(
+        monitor="val/macro avg/f1-score",
+        mode="max",
+        patience=8,
+        min_delta=1e-6
+    )
+]
