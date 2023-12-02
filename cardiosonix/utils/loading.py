5cbd7615334fdf3cf89c9f058901459a90a1d53f
YeaMerci <113313229+YeaMerci@users.noreply.github.com>
2023-12-02 10:40:42 +0300

    fix: func load_from_checkpoint()

--- cardiosonix/utils/loading.py
+++ cardiosonix/utils/loading.py
@@ -17,6 +17,6 @@ def load_from_checkpoint(checkpoint_path: str | Path) -> Module:
     """Load model from checkpoint"""
     if not os.path.exists(checkpoint_path):
         raise FileExistsError(f"Checkpoint with filepath {checkpoint_path} does not exist")
-    if checkpoint_path.split(".")[-1] == "ckpt":
+    if checkpoint_path.split(".")[-1] != "ckpt":
         raise ValueError(f"Checkpoint path must have a 'ckpt' extension, but got {checkpoint_path}")
     return load(checkpoint_path)["hyper_parameters"]["model"]
